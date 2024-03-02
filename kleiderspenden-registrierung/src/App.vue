ff9e7ea45f4cb6ab8a179175dcea039dc6123d8e
Magoka96 <109848508+Magoka96@users.noreply.github.com>
2024-03-02 11:39:25 +0100

    Added routing

--- kleiderspenden-registrierung/src/App.vue
+++ kleiderspenden-registrierung/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <KleiderspendeForm/>
+  <Header></Header>
+     <div class="container-fluid" style="margin-top: 80px;">
+      <router-view/>
+    </div>
 </template>
 
 <script>
-import KleiderspendeForm from './components/KleiderspendeForm.vue'
+
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- kleiderspenden-registrierung/src/main.js
+++ kleiderspenden-registrierung/src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from "vue-router"
 
-createApp(App).mount('#app')
+import KleiderspendeForm from "./components/KleiderspendeForm.vue"
+import About from "./components/About.vue"
+
+const app = createApp(App)
+
+createApp(App)
+
+const routes = [
+    { path: "/Kleiderspende", component: KleiderspendeForm },
+    { path: "/About", component: About }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes,
+    linkActivesClass: "active"
+})
+
+app.use(router)
+
+app.mount('#app')
