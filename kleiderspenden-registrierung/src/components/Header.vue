453966117ddba090cc14df40b03655f0c28375a4
Magoka96 <109848508+Magoka96@users.noreply.github.com>
2024-03-02 12:00:50 +0100

    Modfied Header.vue, added text-light class to router-links

--- kleiderspenden-registrierung/src/components/Header.vue
+++ kleiderspenden-registrierung/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <router-link to="/" class="navbar-brand">Meine Webseite</router-link>
+      <router-link to="/Kleiderspende" class="navbar-brand">Kleiderspenden</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,13 +9,13 @@
       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link to="/Kleiderspende" class="nav-link">Kleiderspende</router-link>
+            <router-link to="/Kleiderspende" class="nav-link text-light">Registrieren</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/About" class="nav-link">Über uns</router-link>
+            <router-link to="/About" class="nav-link text-light">Über uns</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/" class="nav-link">Kontakt</router-link>
+            <router-link to="/" class="nav-link text-light">Kontakt</router-link>
           </li>
         </ul>
       </div>
