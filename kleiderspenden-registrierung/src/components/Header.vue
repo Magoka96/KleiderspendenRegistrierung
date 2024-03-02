c28e0ee140cccb0052a29d8dd6d7c857e7a717b5
Magoka96 <109848508+Magoka96@users.noreply.github.com>
2024-03-02 11:38:26 +0100

    Added Header.vue

---
+++ kleiderspenden-registrierung/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <router-link to="/" class="navbar-brand">Meine Webseite</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/Kleiderspende" class="nav-link">Kleiderspende</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/About" class="nav-link">Über uns</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Kontakt</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
