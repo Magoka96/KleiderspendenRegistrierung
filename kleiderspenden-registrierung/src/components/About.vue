652777069853f156a00b0270ea4c57a76650a8bd
Magoka96 <109848508+Magoka96@users.noreply.github.com>
2024-03-02 11:37:26 +0100

    Added About.vue

---
+++ kleiderspenden-registrierung/src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Über Uns</h2>
+    <p>Hier ist ein kurzer Text, der etwas über uns sagt.</p>
+    <p>Diese Informationen werden noch befüllt.</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'About',
+  }
+</script>
