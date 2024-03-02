a330d64c03ebc8d3d9f7b3fa8d47aabfbe72dd20
Magoka96 <109848508+Magoka96@users.noreply.github.com>
2024-03-02 12:46:06 +0100

    Added a if condition to distinguish collection and personal delivery, removed first and lastname input field

--- kleiderspenden-registrierung/src/components/ClothingDonationForm.vue
+++ kleiderspenden-registrierung/src/components/ClothingDonationForm.vue
@@ -8,58 +8,49 @@
             <label class="col-sm-3 col-form-label">Art der Übergabe:</label>
             <div class="col-sm-9">
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="checkbox1" v-model="collection">
-                <label class="form-check-label" for="checkbox1">Abholung</label>
+                <input class="form-check-input" type="checkbox" id="collection" v-model="formData.collection">
+                <label class="form-check-label" for="collection">Abholung</label>
               </div>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="checkbox2" v-model="personalDelivery">
-                <label class="form-check-label" for="checkbox2">Persönlich Übergabe an der Geschäftsstelle</label>
+                <input class="form-check-input" type="checkbox" id="personalDelivery" v-model="formData.personalDelivery">
+                <label class="form-check-label" for="personalDelivery">Persönliche Übergabe an der Geschäftsstelle</label>
               </div>
             </div>
           </div>
-          <div class="form-group row mb-3">
-            <label for="firstname" class="col-sm-3 col-form-label">Vorname:</label>
-            <div class="col-sm-9">
-              <input type="text" id="firstname" class="form-control" v-model="formData.firstname">
+          <div v-if="formData.personalDelivery || formData.collection">
+              <div class="form-group row mb-3">
+              <label for="typeOfClothe" class="col-sm-3 col-form-label">Art der Kleidung:</label>
+              <div class="col-sm-9">
+                <input type="text" id="typeOfClothe" class="form-control" v-model="formData.typeOfClothe">
+              </div>
+            </div>
+            <div class="form-group row mb-3">
+              <label for="crisisArea" class="col-sm-3 col-form-label">Krisengebiet:</label>
+              <div class="col-sm-9">
+                <input type="text" id="crisisArea" class="form-control" v-model="formData.crisisArea">
+              </div>
             </div>
           </div>
-          <div class="form-group row mb-3">
-            <label for="lastname" class="col-sm-3 col-form-label">Nachname:</label>
-            <div class="col-sm-9">
-              <input type="text" id="lastname" class="form-control" v-model="formData.lastname">
+          <div v-if="formData.collection">
+            <div class="form-group row mb-3">
+              <label for="steetAndNumber" class="col-sm-3 col-form-label">Straße und Hausnummer:</label>
+              <div class="col-sm-9">
+                <input type="text" id="steetAndNumber" class="form-control" v-model="formData.streedAndNumber">
+              </div>
+            </div>
+            <div class="form-group row mb-3">
+              <label for="city" class="col-sm-3 col-form-label">Stadt</label>
+              <div class="col-sm-9">
+                <input type="text" id="city" class="form-control" v-model="formData.city">
+              </div>
+            </div>
+            <div class="form-group row mb-3">
+              <label for="postalCode" class="col-sm-3 col-form-label">Postleitzahl:</label>
+              <div class="col-sm-9">
+                <input type="number" id="postalCode" class="form-control" v-model="formData.postalCode">
+              </div>
             </div>
           </div>
-          <div class="form-group row mb-3">
-            <label for="steetAndNumber" class="col-sm-3 col-form-label">Straße und Hausnummer:</label>
-            <div class="col-sm-9">
-              <input type="text" id="steetAndNumber" class="form-control" v-model="formData.streedAndNumber">
-            </div>
-          </div>
-          <div class="form-group row mb-3">
-            <label for="city" class="col-sm-3 col-form-label">Stadt</label>
-            <div class="col-sm-9">
-              <input type="text" id="city" class="form-control" v-model="formData.city">
-            </div>
-          </div>
-          <div class="form-group row mb-3">
-            <label for="postalCode" class="col-sm-3 col-form-label">Postleitzahl:</label>
-            <div class="col-sm-9">
-              <input type="number" id="postalCode" class="form-control" v-model="formData.postalCode">
-            </div>
-          </div>
-          <div class="form-group row mb-3">
-            <label for="typeOfClothe" class="col-sm-3 col-form-label">Art der Kleidung:</label>
-            <div class="col-sm-9">
-              <input type="text" id="typeOfClothe" class="form-control" v-model="formData.typeOfClothe">
-            </div>
-          </div>
-          <div class="form-group row mb-3">
-            <label for="crisisArea" class="col-sm-3 col-form-label">Krisengebiet:</label>
-            <div class="col-sm-9">
-              <input type="text" id="crisisArea" class="form-control" v-model="formData.crisisArea">
-            </div>
-          </div>
-
           <button type="submit" class="btn btn-primary btn-block">Spende registrieren</button>
         </form>
       </div>
@@ -76,8 +67,6 @@
         formData: {
           collection: false,
           personalDelivery: false,
-          firstname: '',
-          lastname: '',
           streedAndNumber: '',
           city: '',
           postalCode: '',
