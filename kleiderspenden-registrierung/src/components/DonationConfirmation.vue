261483d9476213ec066ccf98376f1ae7fde3b2e9
unknown <gorenflo.marvin@web.de>
2024-03-09 11:42:35 +0100

    Removed first div

--- kleiderspenden-registrierung/src/components/DonationConfirmation.vue
+++ kleiderspenden-registrierung/src/components/DonationConfirmation.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="position-fixed container-fluid d-flex align-items-center vh-100 top-0 start-0 bg-black bg-opacity-50">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card-body border border-black rounded-4 p-3 bg-white">
-            <h1 class="card-title text-center"><strong>Bestätigung</strong></h1>
-            <h3 class="card-title text-center mb-4">Vielen Dank für deine Spende!</h3>
-            <p><strong>Art der Übergabe:</strong> {{ formData.selectedHandoverOption }} </p>
-            <p><strong>Adresse:</strong></p>
-            <div v-if="formData.collection">
-              <div>
-                <p>{{ formData.streedAndNumber }}</p>
-                <p>{{ formData.city }}, {{ formData.postalCode }}</p>
-              </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card-body border border-black rounded-4 p-3 bg-white">
+          <h1 class="card-title text-center"><strong>Bestätigung</strong></h1>
+          <h3 class="card-title text-center mb-4">Vielen Dank für deine Spende!</h3>
+          <p><strong>Art der Übergabe:</strong> {{ formData.selectedHandoverOption }} </p>
+          <p><strong>Adresse:</strong></p>
+          <div v-if="formData.collection">
+            <div>
+              <p>{{ formData.streedAndNumber }}</p>
+              <p>{{ formData.city }}, {{ formData.postalCode }}</p>
             </div>
-            <div v-else>
-              <p>Die-Gütige-Straße 100a</p>
-              <p>Hoffnung, 73123</p>
-            </div>
-            <p><strong>Art der Kleidung:</strong> {{ formData.typeOfClothe }}</p>
-            <p><strong>Krisengebiet:</strong> {{ formData.crisisArea }}</p>
-            <p><strong>Datum und Uhrzeit:</strong> {{ formData.currentDateTime }}</p>
           </div>
+          <div v-else>
+            <p>Die-Gütige-Straße 100a</p>
+            <p>Hoffnung, 73123</p>
+          </div>
+          <p><strong>Art der Kleidung:</strong> {{ formData.typeOfClothe }}</p>
+          <p><strong>Krisengebiet:</strong> {{ formData.crisisArea }}</p>
+          <p><strong>Datum und Uhrzeit:</strong> {{ formData.currentDateTime }}</p>
         </div>
       </div>
     </div>
