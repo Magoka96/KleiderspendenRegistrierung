47e32b2331b45eeba5316939ccbda1e21bd89122
unknown <gorenflo.marvin@web.de>
2024-03-10 10:17:18 +0100

    Added header tag

--- kleiderspenden-registrierung/src/components/Header.vue
+++ kleiderspenden-registrierung/src/components/Header.vue
@@ -1,27 +1,29 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary border border-black border-bottom fixed-top">
-    <div class="container fluid">
-      <img src="@/assets/iu-logo.png" alt="Logo" class="navbar-brand img-fluid" style="max-height: 60px;">
-      <router-link to="/Kleiderspende/Registrierung" class="navbar-brand">Kleiderspenden</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/Kleiderspende/Registrierung" class="nav-link text-black">Registrieren</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/About" class="nav-link text-black">Über uns</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/Kontakt" class="nav-link text-black">Kontakt</router-link>
-          </li>
-        </ul>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary border border-black border-bottom fixed-top">
+      <div class="container fluid">
+        <img src="@/assets/iu-logo.png" alt="Logo" class="navbar-brand img-fluid" style="max-height: 60px;">
+        <router-link to="/Kleiderspende/Registrierung" class="navbar-brand">Kleiderspenden</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/Kleiderspende/Registrierung" class="nav-link text-black">Registrieren</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/About" class="nav-link text-black">Über uns</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/Kontakt" class="nav-link text-black">Kontakt</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 </template>
 
 <script>
