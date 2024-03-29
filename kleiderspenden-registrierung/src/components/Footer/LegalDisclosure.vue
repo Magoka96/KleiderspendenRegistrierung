99804edcfe4cace337d2f774c58b43d1e8359331
unknown <gorenflo.marvin@web.de>
2024-03-29 16:40:54 +0100

    source for logos

--- kleiderspenden-registrierung/src/components/Footer/LegalDisclosure.vue
+++ kleiderspenden-registrierung/src/components/Footer/LegalDisclosure.vue
@@ -3,6 +3,10 @@
       <h1>Legal Disclosure</h1>
       <p>Dieses Projekt wurde im Rahmen meines Studiums erstellt und dient nur zu Lernzwecken.</p>
     </div>
+    <div>
+      <h6>Quellen für externe Inhalte:</h6>
+      <p>Logos: IU Internationale Hochschule. (n.d.). <i>Logo IU Internationale Hochschule (vertikal)</i> [Logo]. IU Bilddatenbank. https://www.iu.de/news/bilddatenbank/</p>
+    </div>
   </template>
   
   <script>
